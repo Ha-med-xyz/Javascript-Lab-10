ff850d27ab3d6216ad8229f4a9c20b08cf788803
Ha-med-xyz <hassan.rzahmed@outlook.com>
2026-03-25 06:46:26 -0400

    Add comments to base class for readability

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
 class productProperties {
-    // Initialize properties
+    // Initialize product properties
     constructor(name, price, quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
-
+    // Method to return total value of product in stock (quantity * price)
     getTotalValue(){
         return this.price * this.quantity;
     }
-
+    // Method to return a string description of the product
     toString() {
         return `Product, ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`
     }
