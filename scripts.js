cac144f5d6729abd38a6afd83b10f302940c8602
Ha-med-xyz <hassan.rzahmed@outlook.com>
2026-03-25 03:52:30 -0400

    Create toString method

--- scripts.js
+++ scripts.js
@@ -9,4 +9,8 @@ class productProperties {
     getTotalValue(){
         return this.price * this.quantity;
     }
+
+    toString() {
+        return `Product, ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`
+    }
 }
